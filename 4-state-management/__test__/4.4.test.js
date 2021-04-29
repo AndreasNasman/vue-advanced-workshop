31782c53e783bbafd77e0dd7ffb3ac284d7ceab8
Andreas Näsman <andreas.nasman@hibox.tv>
2021-04-29 12:53:13 +0300

    Format all files

--- 4-state-management/__test__/4.4.test.js
+++ 4-state-management/__test__/4.4.test.js
@@ -1,18 +1,18 @@
-require('../../util').createTestCase(__filename, (window, logs, done) => {
-  expect(window.document.getElementById('app').innerHTML).toMatch(
+require("../../util").createTestCase(__filename, (window, logs, done) => {
+  expect(window.document.getElementById("app").innerHTML).toMatch(
     `<div>0</div> <div>0</div> <div>0</div>`
-  )
-  window.$click('button')
+  );
+  window.$click("button");
   setTimeout(() => {
-    expect(window.document.getElementById('app').innerHTML).toMatch(
+    expect(window.document.getElementById("app").innerHTML).toMatch(
       `<div>1</div> <div>1</div> <div>1</div>`
-    )
-    window.$click('button')
+    );
+    window.$click("button");
     setTimeout(() => {
-      expect(window.document.getElementById('app').innerHTML).toMatch(
+      expect(window.document.getElementById("app").innerHTML).toMatch(
         `<div>2</div> <div>2</div> <div>2</div>`
-      )
-      done()
-    }, 0)
-  }, 0)
-})
+      );
+      done();
+    }, 0);
+  }, 0);
+});
